706c012640a4b22da26a8fc96435547d87b1a0b6
Ahmet Ertürk <is_ahmet@outlook.com>
2023-07-15 10:37:13 +0200

    Languages section and CV

--- resume/resumeapp/models.py
+++ resume/resumeapp/models.py
@@ -3,30 +3,24 @@ from ckeditor.fields import RichTextField
 
 
 class Egitim(models.Model):
-    baslik = models.CharField(max_length=100)
+    okul = models.CharField(max_length=100)
     aciklama = RichTextField()
     degree = RichTextField(null=True)
     baslangic_tarihi = models.DateField()
     bitis_tarihi = models.DateField(blank=True, null=True)  # Bitiş tarihi opsiyonel
 
     def __str__(self):
-        return self.baslik
+        return self.okul
 
 class Deneyim(models.Model):
-    baslik = models.CharField(max_length=100)
+    gorev = models.CharField(max_length=100)
     aciklama = RichTextField()
     firma = RichTextField(null=True)
     baslangic_tarihi = models.DateField()
     bitis_tarihi = models.DateField(blank=True, null=True)  # Bitiş tarihi opsiyonel
 
     def __str__(self):
-        return self.baslik
-
-class Dil(models.Model):
-    ad = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.ad
+        return self.gorev
 
 class KisiselBilgiler(models.Model):
     ad = models.CharField(max_length=50)
@@ -35,6 +29,7 @@ class KisiselBilgiler(models.Model):
     hakkimda = RichTextField(null=True)
     dogum_tarihi = models.DateField()
     email = models.EmailField()
+    cv = models.FileField(upload_to="uploads/%Y/%m/%d/", blank=True, null=True)
     telefon = models.CharField(max_length=20)
     adres = models.TextField()
     profile_photo  = models.ImageField(upload_to="media/%Y/%m/%d/", blank=True, null=True)
@@ -44,7 +39,14 @@ class KisiselBilgiler(models.Model):
     linkedin = models.URLField(blank=True, null=True)
     twitter = models.URLField(blank=True, null=True)
     instagram = models.URLField(blank=True, null=True)
-    bilinen_diller = models.ManyToManyField(Dil)
 
     def __str__(self):
         return self.ad + " " + self.soyad
+
+class Dil(models.Model):
+    ad = models.CharField(max_length=50,blank=True, null=True)
+    seviye = models.CharField(max_length=50,blank=True, null=True)
+    sertifika = models.CharField(max_length=50,blank=True, null=True)
+
+    def __str__(self):
+        return self.ad
--- resume/resumeapp/views.py
+++ resume/resumeapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from .models import Egitim, Deneyim, KisiselBilgiler
+from .models import Egitim, Deneyim, KisiselBilgiler, Dil
 
 def cv_anasayfa(request):
     egitimler = Egitim.objects.all().order_by('-baslangic_tarihi')
     deneyimler = Deneyim.objects.all().order_by('-baslangic_tarihi')
     kisisel_bilgiler = KisiselBilgiler.objects.first()
-    return render(request, 'index.html', {'egitimler': egitimler, 'deneyimler': deneyimler, 'kisisel_bilgiler': kisisel_bilgiler})
+    diller = Dil.objects.all()
+    
+    return render(request, 'index.html', {'egitimler': egitimler, 'deneyimler': deneyimler, 'kisisel_bilgiler': kisisel_bilgiler, 'diller': diller})
